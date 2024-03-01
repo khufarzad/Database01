d897ac9bd490f897e31272303c26ec2f1213be80
khufarzad <160780389+khufarzad@users.noreply.github.com>
2024-03-01 11:52:14 +0330

    Second Session of SQL.

---
+++ Initial_Database.sql
@@ -0,0 +1,31 @@
+-- Creating Database
+CREATE DATABASE testdb;
+
+
+-- Use The Created Database
+USE testdb;
+
+
+-- Create A Table In Used Database
+CREATE TABLE Students (
+	ID INT,
+    FName VARCHAR(20),
+    Lname VARCHAR(20),
+    Age INT
+);
+
+
+-- Select All Values in Table
+SELECT * FROM Students;
+
+
+-- Insert Data Into Table
+INSERT INTO Students VALUES
+	(1, 'Parvin', 'Yaghoubi', 25),
+    (2, 'Narges', 'Yaghoubi', 22),
+    (3, 'Amin', 'Modanlou', 18),
+    (4, 'Mahdi', 'Zolfaghari', 26);
+
+-- Insert Another Record In Table
+INSERT INTO Students VALUES (5, 'Amir', 'Jamshidi', 18);
+
